60465994462536497abe7710c3c75107591a88d0
Patrick Meppe <tapmeppe@gmail.com>
2013-04-15 16:18:46 +0200

    The structure has been refined.
    Signed-off-by: Patrick Meppe <tapmeppe@gmail.com>

---
+++ paracore/src/main/scala/net/gumbix/parabioinf/hmm/Viterbi.scala
@@ -0,0 +1,13 @@
+package net.gumbix.parabioinf.hmm
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Patrick Meppe
+ * Project name: scabio
+ * Date: 4/15/13
+ * Time: 3:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Viterbi {
+
+}
