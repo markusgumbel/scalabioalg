86ef9fcdbd2eee9ca84ffa3ef8500fd3e575c922
Markus Gumbel <m.gumbel@hs-mannheim.de>
2017-04-06 11:10:47 +0200

    More work on BWT.

--- core/src/main/scala/net/gumbix/bioinf/string/seq/BTW.scala
+++ core/src/main/scala/net/gumbix/bioinf/string/seq/BTW.scala
@@ -1,37 +1,44 @@
 package net.gumbix.bioinf.string.seq
 
 /**
+  * The Burrows-Wheeler Transform.
+  * @param s The string to be transformed.
   * @author Markus Gumbel (m.gumbel@hs-mannheim.de)
   */
 class BWT(val s: String) {
-  def bwt() = {
-    0
-  }
-
-  val size = s.size
 
   val rotationList = {
-    def rotate(i: Int) = s.substring(i, size) + s.substring(0, i)
-    for (i <- 0 until size) yield (i, rotate(i))
+    def rotate(i: Int) = s.substring(i, s.size) + s.substring(0, i)
+    for (i <- 0 until s.size) yield (i, rotate(i))
   }
 
   val sortedRotationList = rotationList.sortBy(_._2)
 
   val suffixArrayList = {
-    val idx = (0 until size) // Numbers 0, 1, ..., n-1
+    val idx = (0 until s.size) // Numbers 0, 1, ..., n-1
     val b = sortedRotationList.map(_._1) // Indizes.
     idx.zip(b) // Merge them.
   }
 
   val suffixArray = suffixArrayList.map(_._2).toArray
+
+  val transformation = {
+    val bwtList = sortedRotationList.map(_._2).map(s => s(s.size - 1))
+    bwtList.mkString("")
+  }
 }
 
 object BTW {
   def main(args: Array[String]) {
-    val btw = new BWT("foobarfoba")
+    //val btw = new BWT("^BANANA|")
+    val btw = new BWT("googol^")
+    println("rotation list:")
     println(btw.rotationList.mkString(","))
+    println("sorted rotation list:")
     println(btw.sortedRotationList.mkString(","))
+    println("suffix array (list):")
     println(btw.suffixArrayList.mkString(","))
-    // println(btw.suffixArray)
+    println("BWT:")
+    println(btw.transformation)
   }
 }
---
+++ core/src/main/scala/net/gumbix/bioinf/string/seq/BWTInv.scala
@@ -0,0 +1,33 @@
+package net.gumbix.bioinf.string.seq
+
+/**
+  * The inverse Burrows-Wheeler Transform.
+  * Begin of string is "^" and end of string is "|".
+  * @param r Burrows-Wheeler transformed text.
+  * @author Markus Gumbel (m.gumbel@hs-mannheim.de)
+  */
+class BWTInv(val r: String) {
+
+  val transformation = {
+    var l = r.map(c => "").toList
+    for (i <- 0 to r.size) {
+      l = (r.map(c => c.toString) zip l).map(e => e._1 + e._2).toList
+      println(l)
+      l = l.sortBy(s => s)
+      println(l)
+      println("")
+    }
+    val f = l.filter(s => s(r.size - 1) == '|')
+    f.head.substring(0, r.size)
+  }
+}
+
+object BTWInv {
+  def main(args: Array[String]) {
+    val ibtw = new BWTInv("BNN^AA|A")
+    println("r:")
+    println(ibtw.r)
+    println("inverse BTW:")
+    println(ibtw.transformation)
+  }
+}
